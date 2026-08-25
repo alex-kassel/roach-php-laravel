1b709ab498ccf09b31c15472cce2b7e281fcfe66
Alex Kassel <alex@kassel.dev>
2026-08-25 23:21:14 +0200

    chore(tests): configure dynamic test bootstrap for root autoloader

---
+++ tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+$candidates = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../../vendor/autoload.php',
+];
+
+$autoloader = null;
+foreach ($candidates as $candidate) {
+    if (file_exists($candidate)) {
+        $autoloader = require $candidate;
+        break;
+    }
+}
+
+if ($autoloader === null) {
+    throw new RuntimeException('Composer autoloader not found.');
+}
+
+$autoloader->addPsr4('RoachPHP\\Laravel\\', __DIR__ . '/../src');
+$autoloader->addPsr4('RoachPHP\\Laravel\\Tests\\', __DIR__);
